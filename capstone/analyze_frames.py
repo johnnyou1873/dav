8e32a31ad11a92f3986af6584c84e0a41f12d69d
ClaraY05 <clara.w.yee@gmail.com>
2025-05-22 07:06:35 -0700

    made hash table, negative progress kek

--- capstone/analyze_frames.py
+++ capstone/analyze_frames.py
@@ -25,7 +25,7 @@ def plot_tuple_pairs(data):
   plt.grid(True)
   plt.show()
 
-plt.rcParams['agg.path.chunksize'] = 10000
+#plt.rcParams['agg.path.chunksize'] = 10000
 #plot_tuple_pairs(flattend_data)
 
 def tup_to_binary(tup):
@@ -33,7 +33,41 @@ def tup_to_binary(tup):
    bit_string += str(tup[0])
    bit_string += bin(tup[1])[2:]
    return bit_string
+# 2D array of bits of each frame
+vals = []
+for arr in data:
+    arr_bins = []
+    for tup in arr:
+      arr_bins.append(tup_to_binary(tup))
+    vals.append(arr_bins)
+
+# create hash table
+num_bits = 0
+flatvals = list(itertools.chain.from_iterable(vals))
+bin_hash = {}
+count = 0
+for bins in flatvals:
+    if bins not in bin_hash:
+      num_bits += len(bins)
+      bin_hash[bins] = count
+      count += 1
+    else:
+      continue
+print(bin_hash)
+print("TOTAL BITS IN DICT: " + num_bits)
+print("")
+hashed_vals = []
+for val in vals:
+    this_hash = []
+    for bin in val:
+       this_hash.append(bin_hash[bin])
+    hashed_vals.append(this_hash)
+   
 
 with open("./juicy_data.csv", "w") as file:
-    for tup in flattend_data:
-       file.write(tup_to_binary(tup)+'\n')
+    file.truncate(0)  # Truncate the file, effectively clearing its content
+    for nums in hashed_vals:
+       file.write(str(nums)+'\n')
+    #file.write(str(vals))
+    #for tup in flattend_data:
+       #file.write(tup_to_binary(tup)+'\n')
